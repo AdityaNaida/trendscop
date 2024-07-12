bfa1f8b59cf351816d1e403f4639b900238e028e
AdityaNaida <adityanaida@gmail.com>
2024-07-12 18:09:36 +0530

    [ADD] Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ["./views/**/*.ejs", "./public/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
